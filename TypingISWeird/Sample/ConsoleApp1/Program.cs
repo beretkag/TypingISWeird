4669a99fd13c907aafcc62a2814d4e7a413dfaae
Beretka Gellért <93581390+beretkag@users.noreply.github.com>
2022-09-14 12:28:56 +0200

    20220914

---
+++ TypingISWeird/Sample/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            string text = "lorem ipsum dolor sit amet...";
+
+            int number = 420;
+
+            bool variable = true;
+
+            Sample ownclass = new Sample();
+
+            List<char> charList = new List<char>();
+
+
+
+            string a = "A";
+
+            string b = a + "B";
+
+            //azaz
+
+            b = "AB";
+
+            //vagy
+            
+
+
+
+
+
+
+
+            b += "C";
+            text += "hello";
+
+            number += 2;
+
+            variable = !variable;
+
+            ownclass.something += " hello";
+
+            charList.Add('A');
+        }
+    }
+}
